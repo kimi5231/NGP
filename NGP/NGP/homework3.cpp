ce4417a53ef0518fb3867bca7f0a19e382b34df3
kimi5231 <djwjod@naver.com>
2025-09-29 18:17:51 +0900

    과제2 중간저장

--- NGP/NGP/homework3.cpp
+++ NGP/NGP/homework3.cpp
@@ -5,8 +5,6 @@
 
 #pragma comment(lib, "ws2_32")
 
-#define TEST "www.naver.com"
-
 int main(int argc, char* argv[])
 {
 	// 윈속 초기화
@@ -15,21 +13,18 @@ int main(int argc, char* argv[])
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 0;
 
-	for (int i = 0; i< argc; i++)
-		std::cout << argv[i] << std::endl;
-
 	hostent* test = gethostbyname(argv[1]);
-	std::cout << test->h_addr_list[0] << std::endl;
-	in_addr addr;
-	memcpy(&addr, test->h_addr_list[0], sizeof(in_addr));
-	std::cout << "IP 주소: " << inet_ntoa(addr) << std::endl;
+	//hostent* test = gethostbyname("www.example.com");
+	
+	std::cout << test->h_name << std::endl;
 
-	//sockaddr_in addr;
-	inet_ntop(AF_INET, test->h_addr_list[0], &addr.sin_addr);
-	//std::cout << addr.sin_addr << std::endl;
+	sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	inet_pton(test->h_addrtype, test->h_addr_list[0], &addr.sin_addr);
+	std::cout << addr.sin_addr << std::endl;
 
-	/*for(int i = 0; i< 5; i++)
-		std::cout << test->h_addr_list[i] << std::endl;*/
-
+	for (int i = 0; test->h_aliases[i]; i++)
+		std::cout << test->h_aliases[i] << std::endl;
+	
 	return 0;
 }
