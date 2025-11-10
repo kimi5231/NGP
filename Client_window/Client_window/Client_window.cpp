613b05d680f227ba8bdcbe72522496b6e07b382c
Lim Sooyoung <djwjod@naver.com>
2025-11-10 23:02:52 +0900

    과제5 완료

--- Client_window/Client_window/Client_window.cpp
+++ Client_window/Client_window/Client_window.cpp
@@ -1,7 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#include <iostream>
-#include <fstream>
+﻿#include <fstream>
 #include <vector>
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -10,30 +7,28 @@
 #include <shlwapi.h>
 #include "resource.h"
 
-#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "ws2_32")
 #pragma comment(lib, "Shlwapi.lib")
 
-HWND hIp;
 HWND hLabel;
 HWND hProgress;
 HWND hSelectButton;
-HWND hInputButton;
 HWND hCancelButton;
 
 OPENFILENAME fileDlg;
 TCHAR path[MAX_PATH];
 
-DWORD WINAPI ProcessNetwork(LPVOID arg)
+void SendFile()
 {
 	// 윈속 초기화
 	WSADATA wsa;
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return 0;
+		return;
 
 	// clientSocket 생성
 	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (clientSocket == INVALID_SOCKET)
-		return 0;
+		return;
 
 	// connect
 	sockaddr_in addr;
@@ -42,15 +37,10 @@ DWORD WINAPI ProcessNetwork(LPVOID arg)
 	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
 	addr.sin_port = htons(7777);
 	if (connect(clientSocket, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
-		return 0;
+		return;
 
 	// 파일 열기
 	std::ifstream file(path, std::ios::binary);
-	if (!file)
-	{
-		std::cout << "파일을 열 수 없습니다." << std::endl;
-		return 0;
-	}
 
 	// 커서를 파일의 끝으로 이동시켜서 전체 파일 크기 구하기
 	file.seekg(0, std::ios::end);
@@ -70,8 +60,9 @@ DWORD WINAPI ProcessNetwork(LPVOID arg)
 	// 가변 데이터 송신
 	send(clientSocket, fileName, fileNameSize, 0);
 
-	int totalReadByte = 0;
 	std::vector<char> buffer(4096);
+	int totalReadByte = 0;
+	
 	while (!file.eof())
 	{
 		file.read(buffer.data(), buffer.size());
@@ -86,8 +77,8 @@ DWORD WINAPI ProcessNetwork(LPVOID arg)
 		// 가변 데이터 송신
 		send(clientSocket, buffer.data(), len, 0);
 
-		// Procress Bar Udate
-		SendMessage(hProgress, PBM_SETPOS, (static_cast<double>(totalReadByte)/fileSize)*100, 0);
+		// Progress Bar Update
+		SendMessage(hProgress, PBM_SETPOS, (static_cast<double>(totalReadByte) / fileSize) * 100, 0);
 	}
 
 	// 파일 닫기
@@ -98,7 +89,7 @@ DWORD WINAPI ProcessNetwork(LPVOID arg)
 
 	// 윈속 종료
 	WSACleanup();
-	return 0;
+	return;
 }
 
 INT_PTR CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -125,11 +116,11 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (GetOpenFileName(&fileDlg))
 				SetDlgItemText(hDlg, IDC_LABEL, path);
 
-			// Thread 생성
-			CreateThread(NULL, 0, ProcessNetwork, NULL, 0, NULL);
-
 			// Select Button 비활성화
 			EnableWindow(hSelectButton, FALSE);
+
+			// 파일 보내기
+			SendFile();
 			return TRUE;
 		case IDB_CANCEL:
 			EndDialog(hDlg, IDB_CANCEL);
